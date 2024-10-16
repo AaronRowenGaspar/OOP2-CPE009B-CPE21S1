c300344e81ac486c8b05c76bb5d8e87d35ef276f
Aaron Rowen Gaspar <qarogaspar@tip.edu.ph>
2024-10-17 04:12:57 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,38 +1,8 @@
-from OOPIntro_GASPAR.Account import Accounts
-from OOPIntro_GASPAR.ATM import ATM
+from registration import RegistrationForm
+import tkinter as tk
 
-Account1 = Accounts.Accounts(account_number=123456,account_firstname="Royce",
-                             account_lastname="Chua",current_balance = 1000,
-                             address = "Silver Street Quezon City",
-                             email = "roycechua123@gmail.com")
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = RegistrationForm(root)
+    root.mainloop()
 
-print("Account 1")
-print(Account1.account_firstname)
-print(Account1.account_lastname)
-print(Account1.current_balance)
-print(Account1.address)
-print(Account1.email)
-
-print()
-
-Account2 = Accounts.Accounts(account_number=654321,account_firstname="John",
-                             account_lastname="Doe",current_balance = 2000,
-                             address = "GOld Street Quezon City",
-                             email = "johndoe@yahoo.com")
-
-print("Account 2")
-print(Account2.account_firstname)
-print(Account2.account_lastname)
-print(Account2.current_balance)
-print(Account2.address)
-print(Account2.email)
-
-ATM1 = ATM.ATM(1234)
-ATM1.deposit(Account1,500)
-ATM1.check_currentbalance(Account1)
-
-ATM1.deposit(Account2,300)
-ATM1.check_currentbalance(Account2)
-
-ATM1.view_transactionsummary()
-print("ATM Serial Number:", ATM1.serial_number)
